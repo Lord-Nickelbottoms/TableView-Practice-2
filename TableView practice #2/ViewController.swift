13269ffc03494dde5bcca633c8f893ac91726523
Lord-Nickelbottoms <nizaam.h@icloud.com>
2023-07-29 17:18:52 +0200

    First commit

---
+++ TableView practice #2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView practice #2
+//
+//  Created by Nizaam Haffejee on 2023/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
